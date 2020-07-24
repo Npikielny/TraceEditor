c378fe219ce4235d3625352b8b9a80b0efe54bfa
Noah Pikielny <npikielny@gmail.com>
2020-07-24 12:39:09 -0400

    Initial Commit

---
+++ TraceEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TraceEditor
+//
+//  Created by Noah Pikielny on 7/24/20.
+//  Copyright © 2020 Noah Pikielny. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TraceEditorTests/TraceEditorTests.swift
@@ -0,0 +1,34 @@
+//
+//  TraceEditorTests.swift
+//  TraceEditorTests
+//
+//  Created by Noah Pikielny on 7/24/20.
+//  Copyright © 2020 Noah Pikielny. All rights reserved.
+//
+
+import XCTest
+@testable import TraceEditor
+
+class TraceEditorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
